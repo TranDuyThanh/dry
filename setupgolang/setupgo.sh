921405fc3f87bd9055c19a3959400ae38c4cb699
Trần Duy Thanh <coachtranduthanh@gmail.com>
2016-07-06 12:31:48 +0700

    update comment

--- setupgolang/setupgo.sh
+++ setupgolang/setupgo.sh
@@ -7,26 +7,26 @@ source $HOME/.bashrc
 go version
 if [ $? -eq 0  ]; then
      exit 0
- fi
+fi
 
- mkdir golang
- mkdir golang/src
- mkdir golang/pkg
- mkdir golang/bin
+mkdir golang
+mkdir golang/src
+mkdir golang/pkg
+mkdir golang/bin
 
- wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 
- sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
 
- echo >> $HOME/.bashrc
- echo >> $HOME/.bashrc
- echo "# Added by Tran Duy Thanh to setup go" >> $HOME/.bashrc
- echo "export GOPATH=\$HOME/golang" >> $HOME/.bashrc
- echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin" >> $HOME/.bashrc
+echo >> $HOME/.bashrc
+echo >> $HOME/.bashrc
+echo "# Added by user to setup go" >> $HOME/.bashrc
+echo "export GOPATH=\$HOME/golang" >> $HOME/.bashrc
+echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin" >> $HOME/.bashrc
 
- source $HOME/.bashrc
+source $HOME/.bashrc
 
- rm -f go1.6.2.linux-amd64.tar.gz
+rm -f go1.6.2.linux-amd64.tar.gz
 
- go version
- go env
+go version
+go env
